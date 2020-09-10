fa8f8eaabcff1b2898d54b3a059bcc74d9d95570
GabrielRbo <gabrielribeiro1698@gmail.com>
2020-09-10 23:54:41 +0000

    Commits Atrasados

--- back-end/controllers/curso.js
+++ back-end/controllers/curso.js
@@ -1,43 +1,33 @@
 /*
-         OPERAÇÕES BÁSICAS SOBRE DADOS
-
-         1) CREATE (Criação ou Inserção)
-            Cria um novo objeto dentro do banco de dados
-
-         2) RETRIEVE (Recuperar ou Listar)
-            Obter de volta um ou mais objetos preexistentes no banco de dados
-         
-         3) UPDATE
-            Atualizar os dados de um objeto preexistente no banco de dados
-
-         4) DELETE
-            Exclusão de um objeto do banco de dados
-
-         (C)reate + (R)etrieve + (U)pdate + (D)elete = Sigla CRUD
-
-         =====================================================================
-
-         Verbos do protocolo HTTP
-
-         Verbo               Operação
-         POST                CREATE
-         GET                 RETRIEVE
-         PUT                 UPDATE
-         DELETE              DELETE
-
-
+    OPERAÇÕES BÁSICAS SOBRE DADOS
+    1) CREATE (criação ou inserção)
+        Cria um novo objeto dentro do banco de dados
+    2) RETRIEVE (recuperar ou listar)
+        Obter de volta um ou mais objetos preexistentes no banco de dados
+    3) UPDATE
+        Atualizar os dados de um objeto preexistente no banco de dados
+    4) DELETE
+        Exclusão de um objeto do banco de dados
+    (C)reate + (R)etrieve + (U)pdate + (D)elete = sigla CRUD
+    ============================================================
+    Verbos do protocolo HTTP
+    Verbo                   Operação
+    POST                    CREATE
+    GET                     RETRIEVE
+    PUT                     UPDATE
+    DELETE                  DELETE
 */
 
 // Controller é um conjunto de funções associadas às operações sobre dados
 
 const Curso = require('../models/Curso')
 
-const controller = {} // Objeto vazio
+const controller = {}   // Objeto vazio
 
-//Operação CREATE, função novo()
+// Operação CREATE, função novo()
 controller.novo = async (req, res) => {
     // Usa os dados que chegam dentro do body da requisição
-    // e os envia ao banco de dados para a criação de um novo objeto
+    // e os envia o BD para a criação de um novo objeto
     try {
         await Curso.create(req.body)
         // HTTP 201: Created
