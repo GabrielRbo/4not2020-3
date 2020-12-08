40ba7fbe1c8a1b6749d61f6968573c0db64d867e
GabrielRbo <gabrielribeiro1698@gmail.com>
2020-12-08 22:21:11 +0000

    Resetando Front-End

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,24 +1,20 @@
+import { MaterialModule } from './material/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PrincipalComponent } from './ui/principal/principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderBarComponent } from './ui/header-bar/header-bar.component';
-import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PrincipalComponent,
-    HeaderBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
